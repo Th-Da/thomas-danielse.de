fdaf8391afe59e788c8d9eca7ccffbfb415d785e
tdNoobie <thomas.danielse@gmx.de>
2022-10-17 15:42:18 +0200

    working on responsivness 2.0

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -9,6 +9,39 @@ export class MyWorkComponent implements OnInit {
   projects = ['Join', 'El Pollo Loco', 'SpaceXRockets', 'SpaceXRockets'];
   projectUrls = ['join', 'el_pollo_loco', 'sapcexrockets', 'sapcexrockets'];
 
+  projectCard = [
+    {
+      "name": "Join - Kanbanboard",
+      "imgPath": "assets/img/projects/join.png",
+      "url": null,
+      showCard: true,
+      "description": "A Kanban board for projectmanagement. Builded in a team.",
+
+    },
+    {
+      "name": "El Pollo Loco",
+      "imgPath": "assets/img/projects/el_pollo_loco.png",
+      "url": null,
+      showCard: false,
+      "description": "A 2D Jump and Run Game based on object-oriented programming (OOP)",
+    },
+    {
+      "name": "SpaceX rockets",
+      "imgPath": "assets/img/projects/spacexrockets.png",
+      "url": null,
+      showCard: false,
+      "description": "A collection of the rockets from spaceX based on a API",
+    },
+    {
+      "name": "SpaceXRockets",
+      "imgPath": "assets/img/projects/spacexrockets.png",
+      "url": null,
+      showCard: false,
+      "description": "A collection of the rockets from spaceX based on a API",
+    }
+  ];
+
+
 
   showCard: boolean;
 
