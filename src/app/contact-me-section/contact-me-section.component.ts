84d777b7d2b035672866e774f1e336022e03645b
tdNoobie <thomas.danielse@gmx.de>
2022-11-01 13:56:38 +0100

    contact me send_mail works

--- src/app/contact-me-section/contact-me-section.component.ts
+++ src/app/contact-me-section/contact-me-section.component.ts
@@ -1,67 +1,63 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { MatFormFieldAppearance } from '@angular/material/form-field';
-
 
 @Component({
   selector: 'app-contact-me-section',
   templateUrl: './contact-me-section.component.html',
-  styleUrls: ['./contact-me-section.component.scss']
+  styleUrls: ['./contact-me-section.component.scss'],
 })
-
-
 export class ContactMeSectionComponent implements OnInit {
-  @ViewChild("focusTarget") focusTarget: any;
+  @ViewChild('focusTarget') focusTarget: any;
 
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('name') name!: ElementRef;
   @ViewChild('email') email!: ElementRef;
   @ViewChild('message') message!: ElementRef;
-  
+
   isDisabled = false;
 
-  constructor() { 
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  focusTargetClick() {
+    this.focusTarget.nativeElement.focus();
   }
 
-  ngOnInit(): void {
-  }
+  async sendMail() {
+    let name = this.name.nativeElement;
+    let email = this.email.nativeElement;
+    let message = this.message.nativeElement;
+    name.disabled = true;
+    email.disabled = true;
+    message.disabled = true;
+    this.isDisabled = true;
 
-focusTargetClick() {
-  this.focusTarget.nativeElement.focus();
-}
+    let fd = new FormData();
+    fd.append('name', name.value);
+    fd.append('email', email.value);
+    fd.append('message', message.value);
 
-async sendMail() {
-let name = this.name.nativeElement
-let email =  this.email.nativeElement
-let message = this.message.nativeElement
-name.disabled = true;
-email.disabled = true;
-message.disabled = true;
-this.isDisabled = true;
+    console.log(fd);
 
-let fd = new FormData
-fd.append('name', name.value);
-fd.append('email', email.value);
-fd.append('message', message.value);
+    await fetch(
+      'send_mail.php',
 
-  await fetch('https://w01d5c04.kasserver.com/thomas-danielse.de/send_mail.php', 
-  
-    {
-      method: 'POST', 
-    body: fd
-    }
-  );
+      {
+        method: 'POST',
+        body: JSON.stringify(fd),
+      }
+    );
 
-  console.log(fd);
-  
+    console.log(fd);
 
-/*   name.disabled = false;
+    /*   name.disabled = false;
   email.disabled = false;
   message.disabled = false; */
-}
+  }
 
-
-disableButton() {
-  this.isDisabled = true;  
-}
+  disableButton() {
+    this.isDisabled = true;
+  }
 }
