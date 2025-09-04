e4e99d0b9d331c2ed610f67eeed4cf40c7314b13
TD <73753078+Th-Da@users.noreply.github.com>
2025-09-04 16:22:03 +0200

    new url for slack project to avoid confusion with real slack

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, HostListener } from '@angular/core';
-import * as AOS from 'aos';
+import { Component, OnInit, HostListener } from "@angular/core";
+import * as AOS from "aos";
 
 @Component({
-  selector: 'app-my-work',
-  templateUrl: './my-work.component.html',
-  styleUrls: ['./my-work.component.scss'],
+  selector: "app-my-work",
+  templateUrl: "./my-work.component.html",
+  styleUrls: ["./my-work.component.scss"],
 })
 export class MyWorkComponent implements OnInit {
   public screenWidth: any;
@@ -15,50 +15,50 @@ export class MyWorkComponent implements OnInit {
 
   projectCard: any = [
     {
-      name: 'Slack-Clone',
-      imgPath: 'assets/img/projects/slack-clone.jpg',
-      URL: 'https://slack-clone.thomas-danielse.de/get-started',
-      gitURL: 'https://github.com/Th-Da/slack-clone',
+      name: "Slack-Clone",
+      imgPath: "assets/img/projects/slack-clone.jpg",
+      URL: "https://team-talks.thomas-danielse.de/get-started",
+      gitURL: "https://github.com/Th-Da/slack-clone",
       showCard: false,
-      description: 'An Angular app of the popular Slack messenger',
-      framework: 'angular',
+      description: "An Angular app of the popular Slack messenger",
+      framework: "angular",
     },
     {
-      name: 'Join - Kanban Board',
-      imgPath: 'assets/img/projects/join.jpg',
-      URL: 'https://join.thomas-danielse.de',
-      gitURL: 'https://github.com/Th-Da/JOIN.git',
+      name: "Join - Kanban Board",
+      imgPath: "assets/img/projects/join.jpg",
+      URL: "https://join.thomas-danielse.de",
+      gitURL: "https://github.com/Th-Da/JOIN.git",
       showCard: false,
-      description: 'A Kanban board for projectmanagement. Builded in a team.',
-      framework: 'vanillajs',
+      description: "A Kanban board for projectmanagement. Builded in a team.",
+      framework: "vanillajs",
     },
     {
-      name: 'El Pollo Loco',
-      imgPath: 'assets/img/projects/el_pollo_loco.png',
-      URL: 'https://elpolloloco.thomas-danielse.de',
-      gitURL: 'https://github.com/Th-Da/El-Pollo-Loco-.git',
+      name: "El Pollo Loco",
+      imgPath: "assets/img/projects/el_pollo_loco.png",
+      URL: "https://elpolloloco.thomas-danielse.de",
+      gitURL: "https://github.com/Th-Da/El-Pollo-Loco-.git",
       showCard: false,
       description:
-        'A 2D Jump and Run Game based on object-oriented programming.',
-      framework: 'vanillajs',
+        "A 2D Jump and Run Game based on object-oriented programming.",
+      framework: "vanillajs",
     },
     {
-      name: 'Personal homepage',
-      imgPath: 'assets/img/projects/portfolio.jpg',
+      name: "Personal homepage",
+      imgPath: "assets/img/projects/portfolio.jpg",
       URL: null,
-      gitURL: 'https://github.com/Th-Da/thomas-danielse.de.git',
+      gitURL: "https://github.com/Th-Da/thomas-danielse.de.git",
       showCard: false,
-      description: 'My homepage with the portfolio - build with Angular.',
-      framework: 'angular',
+      description: "My homepage with the portfolio - build with Angular.",
+      framework: "angular",
     },
     {
-      name: 'SpaceX rockets',
-      imgPath: 'assets/img/projects/spacexrockets.png',
-      URL: 'http://rocketx.thomas-danielse.de/',
-      gitURL: 'https://github.com/Th-Da/spaceXRocket.git',
+      name: "SpaceX rockets",
+      imgPath: "assets/img/projects/spacexrockets.png",
+      URL: "http://rocketx.thomas-danielse.de/",
+      gitURL: "https://github.com/Th-Da/spaceXRocket.git",
       showCard: false,
-      description: 'A collection of the rockets from SpaceX based on an API.',
-      framework: 'vanillajs',
+      description: "A collection of the rockets from SpaceX based on an API.",
+      framework: "vanillajs",
     },
   ];
 
@@ -76,13 +76,13 @@ export class MyWorkComponent implements OnInit {
     AOS.init();
   }
 
-  @HostListener('window:resize', ['$event'])
+  @HostListener("window:resize", ["$event"])
   onResize(event: any) {
     this.getWindowSize();
   }
 
   showFramework(framework: any) {
-    if (framework === 'all') {
+    if (framework === "all") {
       this.subcard = this.projectCard;
     } else {
       const frame = this.projectCard.filter((subCard: any) => {
@@ -104,6 +104,6 @@ export class MyWorkComponent implements OnInit {
   }
 
   openProjectLink(url: string) {
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   }
 }
